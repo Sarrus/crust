983555bb0bd6458be209a4a75165a3704706ef82
Michael Bell <michael@tertheya.com>
2023-04-08 17:35:42 +0100

    Added TCP connection

--- node.c
+++ node.c
@@ -7,6 +7,10 @@
 #include <errno.h>
 #include <poll.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include "node.h"
 #include "terminal.h"
 #include "options.h"
@@ -197,6 +201,30 @@ _Noreturn void crust_node_run()
 
         pollList[i].events = POLLIN;
     }
+
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketFD == -1)
+    {
+        crust_terminal_print("Unable to create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, '\0', sizeof(struct sockaddr_in));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = crustOptionIPAddress;
+    serverAddress.sin_port = htons(crustOptionPort);
+    if(connect(socketFD, (struct sockaddr *)&serverAddress, sizeof(struct sockaddr_in)))
+    {
+        crust_terminal_print("Error connecting to CRUST server.");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a socket
+    // Configure the socket address
+    // Connect the socket
+    // Retry if the connection fails
+    // Reconnect on hangup
 
     crust_node_loop(listLength, pinMap, pollList);
 }
